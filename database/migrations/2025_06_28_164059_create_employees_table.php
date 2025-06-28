213b6a6488478b198f842183f481336ab4dd62b2
anwar7736 <anwarhossain7736@gmail.com>
2025-06-28 22:42:49 +0600

    Create a new employees table

--- database/migrations/2025_06_28_164059_create_employees_table.php
+++ database/migrations/2025_06_28_164059_create_employees_table.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('customers', function (Blueprint $table) {
+        Schema::create('employees', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('customer_no');
+            $table->string('employee_no');
             $table->timestamps();
             $table->softDeletes();
         });
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('customers');
+        Schema::dropIfExists('employees');
     }
 };
